476595d5794ed578ba050ab4b9a657aec325805b
ruibin <ruibinch@gmail.com>
2019-04-01 23:35:03 +0200

    Breakdown into 3 endpoints - cpf/contribution, cpf/allocation, cpf/projection

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
-from logic.cpf import calculate_future_cpf_balance
+from logic.cpf import calculate_cpf_contribution
+from logic.cpf import calculate_cpf_projection
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,24 +10,38 @@ api = Api(app)
 # for data validation
 parser = reqparse.RequestParser()
 parser.add_argument('age', type=int, help='Age')
-parser.add_argument('salary', type=int, help='Current salary')
+parser.add_argument('salary', type=float, help='Annual salary')
+parser.add_argument('bonus', type=float, help='Bonus/commission received in the year')
 parser.add_argument('yoy_increase', type=float, help='Projected YoY increase of salary')
 parser.add_argument('base_cpf', type=list, help='Base amount in CPF accounts')
 parser.add_argument('n_years', type=int, help='Number of years into the future')
 
+class CpfContribution(Resource):
+    def post(self):
+        args = parse.parse_args()
+        cont_employee, cont_employer = calculate_cpf_contribution(args['age'],
+                                                                  args['salary'],
+                                                                  args['bonus'])
+        return { 'cont_employee': cont_employee, 'cont_employer': cont_employer }
 
-class Cpf(Resource):
+class CpfAllocation(Resource):
+    def post(self):
+
+
+class CpfProjection(Resource):
     def post(self):
         args = parser.parse_args()
-        oa, sa, ma = calculate_future_cpf_balance(args['age'],
-                                                  args['salary'],
-                                                  args['yoy_increase'],
-                                                  args['base_cpf'],
-                                                  args['n_years'])
+        oa, sa, ma = calculate_cpf_projection(args['age'],
+                                              args['salary'],
+                                              args['yoy_increase'],
+                                              args['base_cpf'],
+                                              args['n_years'])
 
         return { 'oa': oa, 'sa': sa, 'ma': ma }
 
-api.add_resource(Cpf, '/cpf')
+api.add_resource(CpfContribution, '/cpf/contribution')
+api.add_resource(CpfAllocation, '/cpf/allocation')
+api.add_resource(CpfProjection, '/cpf/projection')
 
 if (__name__ == '__main__'):
     app.debug = True
